6f88dc1a479ac46fd6c021b3fbf08154db7faa63
Nishant Joshi <51093026+NishantJoshi00@users.noreply.github.com>
2021-10-08 15:33:34 +0530

    inception added

--- model.py
+++ model.py
@@ -1,4 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras import models, layers
+from tensorflow.keras.applications import InceptionV3
+
+INPUT_SHAPE = (500, 500, 3)
+
+inception = InceptionV3(include_top=False, weights='imagenet', input_shape=INPUT_SHAPE)
 
 
+model = models.Sequential([
+    inception, 
+    ...
+])
