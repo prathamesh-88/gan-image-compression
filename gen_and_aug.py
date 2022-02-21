6ad365989c85bb53a95354c29b851ab7774708fb
Prathamesh <prathameshtamanekar@gmail.com>
2022-02-21 13:35:41 +0530

    Augmentor corrected

--- gen_and_aug.py
+++ gen_and_aug.py
@@ -5,13 +5,6 @@ from tensorflow.keras.preprocessing.image import img_to_array
 def datagen(path, image_size):
     train_datagen = ImageDataGenerator(
         rescale=1./255,
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest',
     )
 
     train_generator = train_datagen.flow_from_directory(
